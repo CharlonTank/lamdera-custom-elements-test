03205813c57c85a1df7efe9ac40610a28e45a288
Charlon <charles.assus@gmail.com>
2024-07-26 13:15:08 +0700

    Make the SSCE more similare the the comparison

--- elm-pkg-js/editable-stuff.js
+++ elm-pkg-js/editable-stuff.js
@@ -14,8 +14,8 @@ class EditableStuff extends HTMLElement {
     }
 
     set elmElement(newValue) {
+        console.log("Getting new value in JS")
         if (newValue) {
-            console.log("Getting new value in JS")
             console.log(newValue)
         }
     }
